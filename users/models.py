b60e208938af08445f9f880ae678bb246e90df95
MateoJaramillo <mateo.jaramillo.costa@udla.edu.ec>
2025-10-12 17:00:17 -0500

    Creacion del modelo de usuarios

--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+class UsuarioManager(BaseUserManager):
+    def create_user (self, nombre_usuario, email_usuario, contrasenia_usuario=None):
+        if not email_usuario:
+            raise ValueError("No existe el correo de usuario o no es valido")
+        
+        email_usuario = self.normalize_email(email_usuario)
+        user = self.model(nombre_usuario=nombre_usuario, email_usuario = email_usuario)
+        user.set_password = (contrasenia_usuario)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser (self, nombre_usuario, email_usuario, contrasenia_usuario = None):
+        user = self.create_user(nombre_usuario, email_usuario, contrasenia_usuario)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+class Usuario(AbstractBaseUser):
+    nombre_usuario = models.CharField(max_length=50, unique=True)
+    email_usuario = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default= False)
+    
+    objects = UsuarioManager
+    
+    USERNAME_FIELD = 'nombre_usuario'
+    REQUIRED_FIELDS = 'email_usuario'
+    
+    def __str__(self):
+        return self.nombre_usuario
+    
+    def is_staff(self):
+        return self.is_admin
