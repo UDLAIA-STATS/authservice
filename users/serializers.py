026d1e211c337059d573fcc5b3e2e2e6cb80a9d0
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2025-10-15 00:12:25 +0000

    Cambio en el serializer de django rest para la validacion de loggeo por roles

--- users/serializers.py
+++ users/serializers.py
@@ -1,21 +1,29 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from django.db import IntegrityError
 from .models import Usuario
+from django.contrib.auth import authenticate
 
 class RegistroUsuarioSerializer(serializers.ModelSerializer):
     contrasenia_usuario = serializers.CharField(write_only=True)
 
     class Meta:
         model = Usuario
-        fields = ['nombre_usuario', 'email_usuario', 'contrasenia_usuario']
+        fields = ['id', 'nombre_usuario', 'email_usuario', 'contrasenia_usuario', 'rol']
+        read_only_fields = ['id']
 
     def create(self, validated_data):
-        user = Usuario.objects.create_user(
-            nombre_usuario=validated_data['nombre_usuario'],
-            email_usuario=validated_data['email_usuario'],
-            contrasenia_usuario=validated_data['contrasenia_usuario']
-        )
-        return user
+        try:
+            user = Usuario.objects.create_user(
+                nombre_usuario=validated_data['nombre_usuario'],
+                email_usuario=validated_data['email_usuario'],
+                contrasenia_usuario=validated_data['contrasenia_usuario'],
+                rol=validated_data.get('rol', 'profesor')
+            )
+            return user
+        except ValueError as e:
+            raise serializers.ValidationError({'error': str(e)})
+        except IntegrityError:
+            raise serializers.ValidationError({'error': 'El usuario o correo ya está registrado.'})
 
 
 class LoginUsuarioSerializer(serializers.Serializer):
@@ -25,9 +33,11 @@ class LoginUsuarioSerializer(serializers.Serializer):
     def validate(self, data):
         user = authenticate(
             request=self.context.get('request'),
-            username=data['nombre_usuario'],  # debe coincidir con USERNAME_FIELD
+            username=data['nombre_usuario'],
             password=data['contrasenia_usuario']
         )
         if not user:
             raise serializers.ValidationError("Usuario o contraseña incorrecta")
+        if not user.is_active:
+            raise serializers.ValidationError("La cuenta está desactivada")
         return user
